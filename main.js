19d1d5f3c5f1484a472086a8240c8c67d172b2df
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2025-06-23 11:30:49 +0300

    Add Field class with print method and test instance

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+  constructor(field) {
+    this.field = field;
+  }
+
+  print() {
+    // Print each row as a string
+    for (let row of this.field) {
+      console.log(row.join(''));
+    }
+  }
+}
+
+// Test instance
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.print();
